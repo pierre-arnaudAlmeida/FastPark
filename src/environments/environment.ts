b93fb213f0092970e18ab01aedd50ecc086f283d
pierre-arnaudAlmeida <pierre-arnaud94@hotmail.fr>
2021-05-31 15:14:37 +0200

    Adding Firebase to project
    Ajout de Firebase au projet, modification des fichier d'environnement, en ajoutant les configs de Firebase et des modules nécessaires

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAosGE8LeAiLlzd99i1YX0fNfJT9OzphCA",
+    authDomain: "fastpark-16a52.firebaseapp.com",
+    projectId: "fastpark-16a52",
+    storageBucket: "fastpark-16a52.appspot.com",
+    messagingSenderId: "580666493489",
+    appId: "1:580666493489:web:f14b13cce6163227bce310",
+    measurementId: "G-RL9BNP81LT"
+  }
 };
 
 /*
