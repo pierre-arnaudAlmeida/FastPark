5838e34989023812e4e1d0b19ff45d92f45c7a87
pierre-arnaudAlmeida <pierre-arnaud94@hotmail.fr>
2021-06-09 13:57:59 +0200

    Update ParkUtil
    Fix erreur liée au mapping de l'objet reçu de la base de données

--- src/app/classes/ParkUtil.ts
+++ src/app/classes/ParkUtil.ts
@@ -56,8 +56,8 @@ export class ParkUtil {
                 phoneNumber: payload.get('phoneNumber'),
                 freePlaces: payload.get('freePlaces'),
                 totalPlaces: payload.get('totalPlaces'),
-                creationDate: new Date(payload.get('creationDate')).toISOString(),
-                updateDate: new Date(payload.get('updateDate')).toISOString(),
+                creationDate: new Date(payload.get('creationDate')),
+                updateDate: new Date(payload.get('updateDate')),
             }
         }
         throw new Error("The map of the entity name " + entityName + " does not exist");
