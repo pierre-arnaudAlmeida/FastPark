05ecc9ba39f44547a27043478eea9d0d0865e3c9
pierre-arnaudAlmeida <pierre-arnaud94@hotmail.fr>
2021-06-10 13:22:50 +0200

    Update Routes
    Mise à jour des routes du projet, avec l'ajout du AuthGuard dans les pages concernées

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,50 +1,67 @@
 import { NgModule } from '@angular/core';
+import { AuthGuard } from './guards/auth.guard';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'add-park',
-    loadChildren: () => import('./pages/add-park/add-park.module').then( m => m.AddParkPageModule)
+    loadChildren: () => import('./pages/add-park/add-park.module').then( m => m.AddParkPageModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'edit-park/:id',
-    loadChildren: () => import('./pages/edit-park/edit-park.module').then( m => m.EditParkPageModule)
+    loadChildren: () => import('./pages/edit-park/edit-park.module').then( m => m.EditParkPageModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'settings',
-    loadChildren: () => import('./pages/settings/settings.module').then( m => m.SettingsPageModule)
+    loadChildren: () => import('./pages/settings/settings.module').then( m => m.SettingsPageModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'languages',
-    loadChildren: () => import('./pages/languages/languages.module').then( m => m.LanguagesPageModule)
+    loadChildren: () => import('./pages/languages/languages.module').then( m => m.LanguagesPageModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'display',
-    loadChildren: () => import('./pages/display/display.module').then( m => m.DisplayPageModule)
+    loadChildren: () => import('./pages/display/display.module').then( m => m.DisplayPageModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'authorization',
-    loadChildren: () => import('./pages/authorization/authorization.module').then( m => m.AuthorizationPageModule)
+    loadChildren: () => import('./pages/authorization/authorization.module').then( m => m.AuthorizationPageModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'notifications',
-    loadChildren: () => import('./pages/notifications/notifications.module').then( m => m.NotificationsPageModule)
+    loadChildren: () => import('./pages/notifications/notifications.module').then( m => m.NotificationsPageModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'terms',
-    loadChildren: () => import('./pages/terms/terms.module').then( m => m.TermsPageModule)
+    loadChildren: () => import('./pages/terms/terms.module').then( m => m.TermsPageModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'policy',
-    loadChildren: () => import('./pages/policy/policy.module').then( m => m.PolicyPageModule)
+    loadChildren: () => import('./pages/policy/policy.module').then( m => m.PolicyPageModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'credits',
-    loadChildren: () => import('./pages/credits/credits.module').then( m => m.CreditsPageModule)
+    loadChildren: () => import('./pages/credits/credits.module').then( m => m.CreditsPageModule),
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'run',
