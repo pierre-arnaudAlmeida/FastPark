1d5c1ab66ebe64d4e9ba2f31441d74fdc4b1505e
pierre-arnaudAlmeida <pierre-arnaud94@hotmail.fr>
2021-06-14 18:38:05 +0200

    Update adding modal button in addpark page
    Ajout du bouton pour accéder au modal via la page de création d'un parking

--- src/app/pages/add-park/add-park.page.ts
+++ src/app/pages/add-park/add-park.page.ts
@@ -2,9 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { EntityService } from '../../services/entity.service';
 import { Park } from '../../shared/Park';
 import { ParkUtil } from '../../classes/ParkUtil';
+import { Address } from '../../shared/Address';
+import { AddressUtil } from '../../classes/AddressUtil';
 import { NavController } from '@ionic/angular';
 import { Util } from '../../classes/Util';
 import { Router } from '@angular/router';
+import { ModalController } from '@ionic/angular';
+import { ModalAddAddressPage } from '../../components/modal-add-address/modal-add-address.page';
 
 @Component({
   selector: 'app-add-park',
@@ -21,14 +25,12 @@ export class AddParkPage implements OnInit {
   toastCreationMessage: any;
 
   park: Park = ParkUtil.getEmptyPark();
+  address: Address = AddressUtil.getEmptyAddress();
   
-  constructor(private router: Router, private entityService: EntityService, private navCtrl: NavController) {
+  constructor(private router: Router, private entityService: EntityService, private navCtrl: NavController, private modal: ModalController) {
   }
 
   async ngOnInit() {
-    if (!Util.isUserConnected()) {
-      this.router.navigateByUrl("/login");
-    }
   }
 
   async createPark() {
@@ -41,7 +43,18 @@ export class AddParkPage implements OnInit {
     this.navCtrl.navigateForward('/home');
   }
 
-  addAddress() {
+  async addAddress() {
+    const modalAddAddress = await this.modal.create({
+      component: ModalAddAddressPage
+    });
+    
+    modalAddAddress.onDidDismiss().then((address_created) => {
+      if (address_created !== null) {
+        this.address = address_created.data;
+      }
+    });
+
+    return await modalAddAddress.present();
   }
 
 }
