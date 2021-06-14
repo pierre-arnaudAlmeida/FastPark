3b9932cf362f2ccb85a97763fe95fcf5eefd72bc
pierre-arnaudAlmeida <pierre-arnaud94@hotmail.fr>
2021-06-14 16:04:09 +0200

    Update adding arrow back profile page
    Ajout d'une flèche de retour dans la page profile

--- src/app/pages/profile/profile-routing.module.ts
+++ src/app/pages/profile/profile-routing.module.ts
@@ -4,10 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { ProfilePage } from './profile.page';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: ProfilePage
-  }
+  { path: '', component: ProfilePage }
 ];
 
 @NgModule({
