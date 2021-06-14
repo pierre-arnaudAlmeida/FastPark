2285eec4e04c9f349394f6912ce619d652967d9f
pierre-arnaudAlmeida <pierre-arnaud94@hotmail.fr>
2021-06-14 16:15:57 +0200

    Update adding bottom bar
    Ajout d'une bottom bar et fix bug popup

--- src/app/pages/home/home-routing.module.ts
+++ src/app/pages/home/home-routing.module.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePage } from './home.page';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: HomePage,
-  }
+  { path: '', component: HomePage },
+  { path: 'home', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'add-park', redirectTo: '/add-park', pathMatch: 'full' },
+  { path: 'profile', redirectTo: '/profile', pathMatch: 'full' },
+  { path: 'settings', redirectTo: '/settings', pathMatch: 'full' }
 ];
 
 @NgModule({
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { TranslateService } from '@ngx-translate/core';
 import { EntityService } from '../../services/entity.service';
 import { Park } from '../../shared/Park';
 import { ParkUtil } from '../../classes/ParkUtil';
 import { Util } from '../../classes/Util';
 import { property } from '../../app.property';
+
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -17,15 +18,32 @@ export class HomePage implements OnInit {
   allParks = [];
   park: Park = ParkUtil.getEmptyPark();
 
-  constructor(private router: Router, private entityService: EntityService, translateS: TranslateService) {
+  constructor(private router: Router, private entityService: EntityService, public alertController: AlertController) {
   }
 
   async ngOnInit() {
     if (!Util.isUserConnected()) {
       this.router.navigateByUrl("/login");
     }
+    
     await this.entityService.getAll(property.collectionName.parks).subscribe(data => {
       this.allParks = ParkUtil.mapCollection(data, property.collectionName.parks);
     });
   }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
+
 }
