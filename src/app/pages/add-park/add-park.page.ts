95fa0cf29608a4f3c430b1cef3bc9db856f5c62a
pierre-arnaudAlmeida <pierre-arnaud94@hotmail.fr>
2021-06-15 17:37:06 +0200

    Update AddPark page
    Modification de la création d'un parking en fonction de l'utilisateur et de l'adresse et quand toutes les infos nécessaires sont renseignés

--- src/app/pages/add-park/add-park.page.ts
+++ src/app/pages/add-park/add-park.page.ts
@@ -4,9 +4,8 @@ import { Park } from '../../shared/Park';
 import { ParkUtil } from '../../classes/ParkUtil';
 import { Address } from '../../shared/Address';
 import { AddressUtil } from '../../classes/AddressUtil';
-import { NavController } from '@ionic/angular';
-import { Router } from '@angular/router';
-import { ModalController } from '@ionic/angular';
+import { NavController, AlertController, ModalController } from '@ionic/angular';
+import { TranslateService } from '@ngx-translate/core';
 import { ModalAddAddressPage } from '../../components/modal-add-address/modal-add-address.page';
 import { ModalAddManagerPage } from '../../components/modal-add-manager/modal-add-manager.page';
 
@@ -18,30 +17,54 @@ import { ModalAddManagerPage } from '../../components/modal-add-manager/modal-ad
 })
 export class AddParkPage implements OnInit {
 
-  alertNewGroupHeader: any;
-  alertLaunchGroupHeader: any;
-  alertNewGroupTitle: any;
-  alertCancel: any;
-  alertValid: any;
-  toastCreationMessage: any;
+  alertAddressErrorTitle: any;
+  alertAddressErrorMessage: any;
+  alertManagerErrorTitle: any;
+  alertManagerErrorMessage: any;
+  alertFieldsErrorTitle: any;
+  alertFieldsErrorMessage: any;
+  alertPlaceNumberErrorTitle: any;
+  alertPlaceNumberErrorMessage: any;
 
   park: Park = ParkUtil.getEmptyPark();
   address: Address = AddressUtil.getEmptyAddress();
   
-  constructor(private router: Router, private entityService: EntityService, private navCtrl: NavController, private modal: ModalController) {
+  constructor(private entityService: EntityService, private navCtrl: NavController, private modal: ModalController, public alertController: AlertController, translateS: TranslateService) {
+    translateS.get('PARK.address-error-title').subscribe((value: any) => { this.alertAddressErrorTitle = value; });
+    translateS.get('PARK.address-error-message').subscribe((value: any) => { this.alertAddressErrorMessage = value; });
+    translateS.get('PARK.manager-error-title').subscribe((value: any) => { this.alertManagerErrorTitle = value; });
+    translateS.get('PARK.manager-error-message').subscribe((value: any) => { this.alertManagerErrorMessage = value; });
+    translateS.get('PARK.fields-error-title').subscribe((value: any) => { this.alertFieldsErrorTitle = value; });
+    translateS.get('PARK.fields-error-message').subscribe((value: any) => { this.alertFieldsErrorMessage = value; });
+    translateS.get('PARK.place-number-error-title').subscribe((value: any) => { this.alertPlaceNumberErrorTitle = value; });
+    translateS.get('PARK.place-number-error-message').subscribe((value: any) => { this.alertPlaceNumberErrorMessage = value; });
   }
 
   async ngOnInit() {
   }
 
   async createPark() {
-    //TODO Ajouter des vérification pour les différents champs et afficher un message d'erreur
-    await this.entityService.create(this.park, ParkUtil.parkCollectionName).then(
-      id => {
-        this.park.id = id;
-      });
-    this.park = ParkUtil.getEmptyPark();
-    this.navCtrl.navigateForward('/home');
+    //TODO Créer un vérificateur de numero de téléphone
+    if (this.park.addressId === "" || this.park.addressId === null) {
+      await this.showAlertMessage(this.alertAddressErrorTitle, this.alertAddressErrorMessage);
+    }
+    else if (this.park.managerId === "" || this.park.managerId === null) {
+      await this.showAlertMessage(this.alertManagerErrorTitle, this.alertManagerErrorMessage);
+    }
+    else if (this.park.name.trim().length== 0) {
+      await this.showAlertMessage(this.alertFieldsErrorTitle, this.alertFieldsErrorMessage);
+    }
+    else if (this.park.freePlaces > this.park.totalPlaces) {
+      await this.showAlertMessage(this.alertPlaceNumberErrorTitle, this.alertPlaceNumberErrorMessage);
+    }
+    else {
+      await this.entityService.create(this.park, ParkUtil.parkCollectionName).then(
+        id => {
+          this.park.id = id;
+        });
+      this.park = ParkUtil.getEmptyPark();
+      this.navCtrl.navigateForward('/home');
+    }
   }
 
   async addManager() {
@@ -54,8 +77,6 @@ export class AddParkPage implements OnInit {
     
     modalAddManager.onDidDismiss().then((manager_selected) => {
       if (manager_selected !== null && manager_selected.data.id !== "") {
-        console.log(manager_selected.data);
-        console.log(manager_selected.data.id);
         this.park.managerId = manager_selected.data.id
       }
     });
@@ -76,10 +97,19 @@ export class AddParkPage implements OnInit {
           id => {
             this.address.id = id;
           });
+        this.park.addressId = this.address.id;
       }
     });
 
     return await modalAddAddress.present();
   }
 
+  async showAlertMessage(title: string, message: string) {
+    const alert = await this.alertController.create({
+      header: title,
+      message: message,
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
 }
