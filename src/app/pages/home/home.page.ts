024d32f504655f859582afb3748269a9ac310266
gaspard <gaspard.tertrais@gmail.com>
2021-09-20 13:10:53 +0200

    add distance function, not display if < 5 km

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -94,6 +94,27 @@ export class HomePage implements OnInit {
     await alert.present();
   }
 
+  distance(lat1, lon1, lat2, lon2) {
+	if ((lat1 == lat2) && (lon1 == lon2)) {
+		return 0;
+	}
+	else {
+		var radlat1 = Math.PI * lat1/180;
+		var radlat2 = Math.PI * lat2/180;
+		var theta = lon1-lon2;
+		var radtheta = Math.PI * theta/180;
+		var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+		if (dist > 1) {
+			dist = 1;
+		}
+		dist = Math.acos(dist);
+		dist = dist * 180/Math.PI;
+		dist = dist * 60 * 1.1515;
+		dist = dist * 1.609344 // miles en Km
+		return dist;
+	}
+}
+
   ionViewDidEnter() { this.leafletMap(); }
 
   leafletMap() {
@@ -103,7 +124,10 @@ export class HomePage implements OnInit {
     }).addTo(this.map);
 
 	this.allAddresses.map( address =>{
-		Leaflet.marker([address.position._lat, address.position._long]).addTo(this.map);
+		let distance = this.distance(address.position._lat, address.position._long, this.latitude, this.longitude);
+		if(distance <= 5){
+			Leaflet.marker([address.position._lat, address.position._long]).addTo(this.map);
+		}		
 	});
 	
 
