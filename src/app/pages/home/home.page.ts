303b22d13c8884b43eab0cbc93a4f24e27023a04
Martin <martin@debian.none>
2021-10-01 09:39:19 +0200

    Update

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -157,62 +157,63 @@ export class HomePage implements OnInit {
   });
   
     Leaflet.marker([this.latitude, this.longitude], {icon: redIcon}).addTo(this.map).bindPopup('My Position').openPopup();
+    
+    // Choose park with direction on click
+    // parkMarker.on('click', function(e){
+    //   var angular: any
+    //   var app = angular.module('plunker', ['google-maps']);
 
-    parkMarker.on('click', function(e){
-      var angular: any
-      var app = angular.module('plunker', ['google-maps']);
-
-      app.controller('MainCtrl', function($scope, $document) {
-        // map object
-        $scope.map = {
-          control: {},
-          center: {
-              latitude: -37.812150,
-              longitude: 144.971008
-          },
-          zoom: 14
-        };
+    //   app.controller('MainCtrl', function($scope, $document) {
+    //     // map object
+    //     $scope.map = {
+    //       control: {},
+    //       center: {
+    //           latitude: -37.812150,
+    //           longitude: 144.971008
+    //       },
+    //       zoom: 14
+    //     };
         
-        // marker object
-        $scope.marker = {
-          center: {
-              latitude: -37.812150,
-              longitude: 144.971008
-          }
-        }
+    //     // marker object
+    //     $scope.marker = {
+    //       center: {
+    //           latitude: -37.812150,
+    //           longitude: 144.971008
+    //       }
+    //     }
         
-        // instantiate google map objects for directions
-        var directionsDisplay = new google.maps.DirectionsRenderer();
-        var directionsService = new google.maps.DirectionsService();
-        var geocoder = new google.maps.Geocoder();
+    //     // instantiate google map objects for directions
+    //     var directionsDisplay = new google.maps.DirectionsRenderer();
+    //     var directionsService = new google.maps.DirectionsService();
+    //     var geocoder = new google.maps.Geocoder();
         
-        // directions object -- with defaults
-        $scope.directions = {
-          origin: "Collins St, Melbourne, Australia",
-          destination: "MCG Melbourne, Australia",
-          showList: false
-        }
+    //     // directions object -- with defaults
+    //     $scope.directions = {
+    //       origin: "Collins St, Melbourne, Australia",
+    //       destination: "MCG Melbourne, Australia",
+    //       showList: false
+    //     }
         
-        // get directions using google maps api
-        $scope.getDirections = function () {
-          var request = {
-            origin: $scope.directions.origin,
-            destination: $scope.directions.destination,
-            // travelMode: google.maps.DirectionsTravelMode.DRIVING
-          };
-          directionsService.route(request, function (response, status) {
-            if (status === google.maps.DirectionsStatus.OK) {
-              directionsDisplay.setDirections(response);
-              directionsDisplay.setMap($scope.map.control.getGMap());
-              directionsDisplay.setPanel(document.getElementById('directionsList'));
-              $scope.directions.showList = true;
-            } else {
-              alert('Google route unsuccesfull!');
-            }
-          });
-        }
-      });
-    });
+    //     // get directions using google maps api
+    //     $scope.getDirections = function () {
+    //       var request = {
+    //         origin: $scope.directions.origin,
+    //         destination: $scope.directions.destination,
+    //         // travelMode: google.maps.DirectionsTravelMode.DRIVING
+    //       };
+    //       directionsService.route(request, function (response, status) {
+    //         if (status === google.maps.DirectionsStatus.OK) {
+    //           directionsDisplay.setDirections(response);
+    //           directionsDisplay.setMap($scope.map.control.getGMap());
+    //           directionsDisplay.setPanel(document.getElementById('directionsList'));
+    //           $scope.directions.showList = true;
+    //         } else {
+    //           alert('Google route unsuccesfull!');
+    //         }
+    //       });
+    //     }
+    //   });
+    // });
   }
 
   /** Remove map when we have multiple map object */
