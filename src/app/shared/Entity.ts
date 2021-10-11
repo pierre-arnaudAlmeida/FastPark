958b8f8f7354b9bd75eb9ad5181c5ca37fbb3b0b
Martin <martin@debian.none>
2021-10-11 11:42:10 +0200

    Del

--- src/app/shared/Entity.ts
+++
@@ -1,5 +0,0 @@
-// Interface mère de toutes les entités qui possèderont une collection dans Firebase
-// Si une classe ou interface possède une collection sous Firebase alors elle devra hériter de cette interface (Entity)
-export interface Entity {
-
-}
